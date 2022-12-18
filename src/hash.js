eafcbffd29632a76d54451bbb99763125477818f
katusha-2010 <katusha-2010@mail.ru>
2022-12-18 21:08:36 +0300

    feat: add decision for hash

--- src/hash.js
+++ src/hash.js
@@ -5,11 +5,9 @@ import { createHash } from "crypto";
 export async function hashFile(argsAfterCommand) {
   try {
     const pathToFile = path.resolve(argsAfterCommand);
-    console.log(pathToFile);
     const text = await readFile(pathToFile, "utf8");
-
-    // const hash = createHash("sha256").update(text).digest("hex");
-    text.then((val) => process.stdout(val));
+    const hash = createHash("sha256").update(text).digest("hex");
+    console.log(hash);
   } catch (error) {
     console.log("Operation failed");
   }
